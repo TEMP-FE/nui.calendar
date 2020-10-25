35318c7b54d3d6530834f03cbee48e5cbdb4e823
mansub-han <mansub.han@nts-corp.com>
2020-10-25 15:07:11 +0900

    [#9] more 기능 추가

--- src/components/MonthlyCalendar/MonthlyCalendar.js
+++ src/components/MonthlyCalendar/MonthlyCalendar.js
@@ -11,7 +11,7 @@ const DEMO_PROPS = {
 		{
 			title: '일정 1',
 			startAt: new Date(2020, 9, 1),
-			endAt: new Date(2020, 9, 2),
+			endAt: new Date(2020, 9, 5),
 			location: '',
 			category: '',
 			isAllDay: true,
@@ -44,7 +44,7 @@ const DEMO_PROPS = {
 		{
 			title: '일정 4',
 			startAt: new Date(2020, 9, 12),
-			endAt: new Date(2020, 9, 19),
+			endAt: new Date(2020, 9, 22),
 			location: '',
 			category: '',
 			isAllDay: true,
@@ -77,7 +77,41 @@ const DEMO_PROPS = {
 		{
 			title: '일정 7',
 			startAt: new Date(2020, 9, 15),
-			endAt: new Date(2020, 9, 17),
+			endAt: new Date(2020, 9, 19),
+			location: '',
+			category: '',
+			isAllDay: true,
+			isBlocked: false,
+			isPrivate: false,
+			isRepeatable: false,
+		},
+
+		{
+			title: '일정 8',
+			startAt: new Date(2020, 9, 19),
+			endAt: new Date(2020, 9, 23),
+			location: '',
+			category: '',
+			isAllDay: true,
+			isBlocked: false,
+			isPrivate: false,
+			isRepeatable: false,
+		},
+		{
+			title: '일정 9',
+			startAt: new Date(2020, 9, 20),
+			endAt: new Date(2020, 9, 31),
+			location: '',
+			category: '',
+			isAllDay: true,
+			isBlocked: false,
+			isPrivate: false,
+			isRepeatable: false,
+		},
+		{
+			title: '일정 10',
+			startAt: new Date(2020, 9, 15),
+			endAt: new Date(2020, 9, 16),
 			location: '',
 			category: '',
 			isAllDay: true,
@@ -109,17 +143,37 @@ const CalendarHeader = () => {
 }
 
 // 달력 셀
-const CalendarCell = ({ dateTime, isHoliday, isDimmed, moreList }) => {
+const CalendarCell = ({ dateTime, isHoliday, isDimmed, scheduleList }) => {
 	const { year, month, date } = getDateInfo(dateTime)
+	const [moreList, setMoreList] = useState()
 
 	// 셀 클릭 이벤트
-	const onClickCell = () => {
+	const onCellClick = () => {
 		console.log(year, month, date)
 	}
+
+	// 더보기 버튼 클릭 이벤트
+	const onMoreButtonClick = (e) => {
+		e.stopPropagation()
+		console.log('moreList : ', moreList)
+	}
+
+	const filterOverStackSchedule = () => scheduleList.filter((scheduleItem) => scheduleItem.stack > 3)
+
+	useEffect(() => {
+		setMoreList(filterOverStackSchedule())
+	}, [scheduleList])
+
 	return (
-		<div className={cx('calendar_cell')} onClick={onClickCell}>
-			<span className={cx('date', { '-holiday': isHoliday, is_dimmed: isDimmed })}>{date}</span>
-			{moreList > 0 && <button type={'button'}>{moreList} more</button>}
+		<div className={cx('calendar_cell')} onClick={onCellClick}>
+			<div className={cx('cell_header')}>
+				<span className={cx('date', { '-holiday': isHoliday, is_dimmed: isDimmed })}>{date}</span>
+				{moreList && moreList.length > 0 && (
+					<button className={cx('more_button')} type={'button'} onClick={onMoreButtonClick}>
+						{moreList.length} more
+					</button>
+				)}
+			</div>
 		</div>
 	)
 }
@@ -127,14 +181,15 @@ const CalendarCell = ({ dateTime, isHoliday, isDimmed, moreList }) => {
 // 월 달력
 const MonthlyCalendar = ({ year = getDateInfo().year, month = getDateInfo().month }) => {
 	const [scheduleList, setScheduleList] = useState(DEMO_PROPS.scheduleList)
+	const [calendarScheduleList, setCalendarScheduleList] = useState()
 	const [dateInfoList, setDateInfoList] = useState()
 	const currentMonthInfo = getMonthInfo({ year, month })
 	const weekCount = calcWeekCount({ year, month })
-	let scheduleStack = new Array(weekCount).fill(null).map((_) => []) // 일정스택 확인 변수
 
 	// 달력 정보 만들기
 	const makeDateInfoList = () => {
 		const newDateInfoList = new Array(weekCount).fill(null).map((_) => [])
+
 		for (let i = 0; i < weekCount; i++) {
 			for (let j = 0; j < 7; j++) {
 				const date = 1 - currentMonthInfo.firstDayOfWeek + j + i * 7
@@ -142,92 +197,101 @@ const MonthlyCalendar = ({ year = getDateInfo().year, month = getDateInfo().mont
 				const dateInfo = {
 					dateTime,
 					isHoliday: j === 0,
+					scheduleList: Array(0),
+					stack: 0,
 				}
 
 				newDateInfoList[i].push(dateInfo)
 			}
 		}
-		setDateInfoList(newDateInfoList)
-	}
 
-	// 스케줄 위치 계산
-	const calcSchedulePosition = () => {
-		const cellPadding = '10px' // 셀 왼쪽 패딩
-		const dateHeight = '25px' // 셀 날짜 높이
-		const calendarItemHeight = '28px' // 일정 아이템 높이
+		const newScheduleList = ascendingScheduleList(scheduleList).map((scheduleItem) => {
+			let period = calcScheduleDay(scheduleItem)
+			let renderList = []
 
-		setScheduleList(
-			scheduleList.map((scheduleItem) => {
-				let period = calcScheduleDay(scheduleItem)
-				let renderList = []
+			for (let i = 0; i < weekCount; i++) {
+				for (let j = 0; j < 7; j++) {
+					if (isSameDate(newDateInfoList[i][j].dateTime, scheduleItem.startAt)) {
+						// 일정을 넣을 수 있는 stack 값 찾기
+						let stack = 1
+						let isPossible = false
 
-				for (let i = 0; i < weekCount; i++) {
-					for (let j = 0; j < 7; j++) {
-						if (isSameDate(dateInfoList[i][j].dateTime, scheduleItem.startAt)) {
-							const currentStack = scheduleStack[i][j] ? scheduleStack[i][j] + 1 : 1
-							while (period > 0) {
-								const top = `calc(${(100 / weekCount) * i}% + ${
-									currentStack - 1
-								}*${calendarItemHeight} + ${dateHeight} )`
-								const left = `calc(${14.29 * j}% + ${cellPadding})`
+						while (!isPossible) {
+							for (let p = 0; p < period; p++) {
+								const num = i * 7 + j + p
+								const iPos = Math.floor(num / 7)
+								const jPos = num % 7
 
-								if (7 - j < period) {
-									// 스택 업데이트
-									for (let k = j; k < 7; k++) {
-										scheduleStack[i][k] = currentStack
-									}
+								if (
+									newDateInfoList[iPos][jPos].scheduleList.filter(
+										(scheduleItem) => scheduleItem.stack === stack,
+									).length
+								) {
+									stack++
+									break
+								}
+								if (p === period - 1) {
+									isPossible = true
+								}
+							}
+						}
 
-									const width = `calc(${14.29 * (7 - j)}% - 20px)`
-									renderList.push({ top, left, width, stack: currentStack })
-									period = period - (7 - j)
-								} else {
-									// 스택 업데이트
-									for (let k = j; k < j + period; k++) {
-										scheduleStack[i][k] = currentStack
-									}
+						//
+						while (period > 0) {
+							const top = `calc(${(100 / weekCount) * i}% + ${stack - 1}*28px + 25px )`
+							const left = `calc(${14.29 * j}% + 10px)`
 
-									const width = `calc(${period * 14.29}% - 20px)`
-									renderList.push({ top, left, width, stack: currentStack })
-									period = 0
+							if (7 - j < period) {
+								// 스택 업데이트
+								for (let k = j; k < 7; k++) {
+									newDateInfoList[i][k].scheduleList.push({ ...scheduleItem, stack })
+									newDateInfoList[i][k].stack = stack
 								}
 
+								const width = `calc(${14.29 * (7 - j)}% - 20px)`
+								renderList.push({ top, left, width, stack })
+								period = period - (7 - j)
+							} else {
 								// 스택 업데이트
+								for (let k = j; k < j + period; k++) {
+									newDateInfoList[i][k].scheduleList.push({ ...scheduleItem, stack })
+									newDateInfoList[i][k].stack = stack
+								}
 
-								i++
-								j = 0
+								const width = `calc(${period * 14.29}% - 20px)`
+								renderList.push({ top, left, width, stack })
+								period = 0
 							}
 
-							return {
-								...scheduleItem,
-								renderList,
-							}
+							i++
+							j = 0
+						}
+
+						return {
+							...scheduleItem,
+							renderList,
 						}
 					}
 				}
-			}),
-		)
+			}
+		})
 
-		console.log(scheduleStack)
+		console.log(newScheduleList)
+		setCalendarScheduleList(newScheduleList)
+		setDateInfoList(newDateInfoList)
 	}
 
 	// 먼저 시작하는 일정 순서로 정렬
-	const ascendingScheduleList = () => {
-		setScheduleList(scheduleList.sort((a, b) => a.startAt.getTime() - b.startAt.getTime()))
-	}
+	const ascendingScheduleList = (scheduleList) =>
+		scheduleList.sort((a, b) => a.startAt.getTime() - b.startAt.getTime())
 
 	useEffect(() => {
 		makeDateInfoList()
-		ascendingScheduleList()
+		// ascendingScheduleList()
 	}, [])
 
 	useEffect(() => {
-		if (dateInfoList) {
-			calcSchedulePosition()
-		}
-	}, [dateInfoList])
-
-	useEffect(() => {
-		console.log(scheduleList)
+		// console.log(scheduleList)
 	}, [scheduleList])
 
 	return (
@@ -239,24 +303,25 @@ const MonthlyCalendar = ({ year = getDateInfo().year, month = getDateInfo().mont
 				<CalendarHeader />
 				<div className={cx('calendar_content')}>
 					{/* 달력 그리기 */}
-					{dateInfoList?.map((dateInfoRow, i) => (
-						<div key={`row-${i}`} className={cx('calendar_row')}>
-							{dateInfoRow?.map((dateInfoItem, j) => {
-								return (
-									<CalendarCell
-										key={dateInfoItem.dateTime.getTime()}
-										dateTime={dateInfoItem.dateTime}
-										isDimmed={dateInfoItem.dateTime.getMonth() !== month - 1}
-										isHoliday={dateInfoItem.isHoliday}
-										moreList={scheduleStack[i][j] - 3}
-									/>
-								)
-							})}
-						</div>
-					))}
+					{calendarScheduleList &&
+						dateInfoList?.map((dateInfoRow, i) => (
+							<div key={`row-${i}`} className={cx('calendar_row')}>
+								{dateInfoRow?.map((dateInfoItem, j) => {
+									return (
+										<CalendarCell
+											key={dateInfoItem.dateTime.getTime()}
+											dateTime={dateInfoItem.dateTime}
+											isDimmed={dateInfoItem.dateTime.getMonth() !== month - 1}
+											isHoliday={dateInfoItem.isHoliday}
+											scheduleList={dateInfoItem.scheduleList}
+										/>
+									)
+								})}
+							</div>
+						))}
 
 					{/* 일정 그리기  */}
-					{scheduleList.map((scheduleItem) => {
+					{calendarScheduleList?.map((scheduleItem) => {
 						const startAt = getDateInfo(scheduleItem.startAt)
 						const endAt = getDateInfo(scheduleItem.endAt)
 						const startAtString = `${startAt.year}/${startAt.month}/${startAt.date}`
